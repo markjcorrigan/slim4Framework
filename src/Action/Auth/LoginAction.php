a45fabf3c1e6bdfa50793073f7d5efcffbd562b0
Mark Corrigan <markjc@mweb.co.za>
2024-02-01 15:13:54 +0200

    Prettied up login screen for LoginAction.php

--- src/Action/Auth/LoginAction.php
+++ src/Action/Auth/LoginAction.php
@@ -11,11 +11,29 @@ final class LoginAction
         ServerRequestInterface $request,
         ResponseInterface $response
     ): ResponseInterface {
-        $html = '<form action="login" method="POST">
-        Username: <input type="text" name="username"><br>
-        Password: <input type="password" name="password"><br>
-        <input type="submit" value="Login">
-        </form>';
+//        $html = '<form action="login" method="POST">
+//        Username: <input type="text" name="username"><br>
+//        Password: <input type="password" name="password"><br>
+//        <input type="submit" value="Login">
+//        </form>';
+
+        $html = '<h2>Login Page</h2><br>
+    <div class="login">
+    <form  action="login" method="POST" >
+        <label><b>User Name
+        </b>
+        </label>
+        <input type="text" name="username"  placeholder="Username">
+        <br><br>
+        <label><b>Password
+        </b>
+        </label>
+        <input type="password" name="password"  placeholder="Password">
+        <br><br>
+        <input type="submit"  value="Log In Here">
+        <br><br>
+     
+    </form>';
         $response->getBody()->write($html);
 
         return $response;
